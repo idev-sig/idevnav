7c4ead234d9f69603276ca5b54eb2ee0c26bee91
Jetsung Chan <i@jetsung.com>
2023-02-24 13:56:07 +0800

    fix: publish script

--- publish.sh
+++ publish.sh
@@ -6,12 +6,14 @@ set -u
 # hugo build
 hugo -D
 
-PORT="20171"
-[ $# -ge 2 ] && PORT="${2}"
-
-HTTP_PROXY=http://localhost:${PORT} wrangler pages publish docs
-
 # git push
 git add .
 git commit -am "${1}"
 git push origin main
+
+# set port
+PORT="20171"
+[ $# -ge 2 ] && PORT="${2}"
+
+# publish
+HTTP_PROXY=http://localhost:${PORT} wrangler pages publish docs --project-name nav
