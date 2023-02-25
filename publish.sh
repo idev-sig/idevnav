8e7548de991df798335df861601072ad7eb6572d
Jetsung Chan <i@jetsung.com>
2023-02-25 18:52:59 +0800

    feat: publish

--- publish.sh
+++ publish.sh
@@ -7,14 +7,15 @@ set -u
 hugo -D
 
 # git push
-git add .
-git commit -am "feat: ${1}"
-git push origin main
+if [ $# -ge 1 ]; then
+  git add .
+  git commit -am "feat: ${1}"
+  git push origin main
+fi
 
 # set port
 PORT="20171"
 [ $# -ge 2 ] && PORT="${2}"
 
-echo "${PORT}"
 # publish
 HTTP_PROXY=http://localhost:${PORT} wrangler pages publish docs --project-name nav
