2300969613874d4c3469207915770df6ae183580
Jetsung Chan <i@jetsung.com>
2023-03-12 16:02:19 +0800

    feat: update publish.sh

--- publish.sh
+++ publish.sh
@@ -11,12 +11,18 @@ PORT="20171"
 [ $# -ge 2 ] && PORT="${2}"
 
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-PROJECT_NAME="nav"
 
-[[ "${BRANCH_NAME}" = "main" ]] || PROJECT_NAME="navmore"
+PROJECT_NAME="nav"
+PUB_BARNCH_NAME="main"
+PUBLISH_DIR="docs"
+
+if [[ "$BRANCH_NAME" != "$PUB_BARNCH_NAME" ]]; then
+  # PUB_BARNCH_NAME="dev"
+  PROJECT_NAME="navmore"
+fi
 
 # publish
-HTTP_PROXY=http://localhost:${PORT} wrangler pages publish public --project-name "${PROJECT_NAME}"
+HTTP_PROXY=http://localhost:$PORT wrangler pages publish "$PUBLISH_DIR" --project-name "$PROJECT_NAME" --branch "$PUB_BARNCH_NAME"
 
 # git push
 if [ $# -ge 1 ]; then
