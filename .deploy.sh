f29887b4dea0c6917a7a47bd59e2ee525c0fe145
Jetsung Chan <jetsungchan@gmail.com>
2025-02-04 04:54:37 +0800

    feat: 修复 icon sync

--- .deploy.sh
+++ .deploy.sh
@@ -16,6 +16,7 @@ DEPLOY=""      # 部署
 readonly ICON_DIR="static/assets/images/logos"
 readonly WEBSTACK_FILE="./data/webstack.yml"
 readonly SYNC_FILE=".sync.txt"
+readonly SYNC_FILE_ERROR_LOG="$SYNC_FILE.error.log" 
 
 check_command() {
     command -v "$1" >/dev/null 2>&1
@@ -216,6 +217,7 @@ process_icons() {
     printf "Downloading logo: \n  %-40s => %s\n" "$favicon_url" "$cleaned_name"
     if ! curl -fsL -o "$filepath" "$favicon_url"; then
       echo -e "\033[33mWarning: favicon $logo skipped...\033[0m"
+      echo "$logo" >> "$SYNC_FILE_ERROR_LOG"
     fi
   fi
 }
@@ -224,6 +226,10 @@ process_icons() {
 process_webstack() {
   declare -A current_block
   in_block=0
+
+  if [ -f "$SYNC_FILE_ERROR_LOG" ]; then
+    rm -rf "$SYNC_FILE_ERROR_LOG"
+  fi
 
   # 逐行读取文件
   while IFS= read -r line; do
@@ -271,9 +277,8 @@ fetch_icons() {
     mkdir -p "$ICON_DIR"
   fi
   
-  process_webstack
-
   sync_images
+  process_webstack
 }
 
 main() {
